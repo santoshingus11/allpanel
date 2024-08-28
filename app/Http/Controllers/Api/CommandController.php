7d2d2029ae6287b68deaec41cc9089178a166264
santoshingus11 <shahbaziqbal50@gmail.com>
2024-08-28 10:31:43 -0700

    dash api a

--- app/Http/Controllers/Api/CommandController.php
+++ app/Http/Controllers/Api/CommandController.php
@@ -625,6 +625,9 @@ class CommandController extends Controller
     {
         $pending_deposits = Deposit::where('status', 'pending')->get()->count();
         $pending_withdraw = Withdraw::where('status', 'pending')->get()->count();
+        $total_balancedown=QueryHelper::total_balancedown(4,4);
+        $up=QueryHelper::total_balanceup(4,4);
+        $total_balanceup = $up - $total_balancedown;
         return response()->json([
             'status' => 'success',
             'response' => get_defined_vars(),
